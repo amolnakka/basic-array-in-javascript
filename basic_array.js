d8470acdd47669109bb1afd41b3508e6b20077e2
Code Sandbox <hello@codesandbox.io>
2022-01-07 03:01:44 +0000

    Deployment from Codesandbox.

---
+++ basic_array.js
@@ -0,0 +1,39 @@
+const marks = [21, 33, 69, 89, 92, 99];
+const fruits = ["Orange", "Apple", "Mango"];
+const mixed = ["str", 31, [59, 63]];
+
+// print array in console
+// console.log(marks, fruits, mixed);
+
+// change value in array
+// fruits[2] = "Guava";
+// console.log(fruits);
+
+// index value
+// let value = marks.indexOf(69);
+// console.log(value);
+
+// add values in array
+// marks.push(333, 999, "Graps");
+// console.log(marks);
+
+// add values in start
+// marks.unshift(333, 999, "Graps");
+// console.log(marks);
+
+// remove last value
+// marks.pop();
+// console.log(marks);
+
+// remove start value
+// marks.shift();
+// console.log(marks);
+
+// values are remove as per requirment
+//(index 1 with next 2 values are remove)
+// marks.splice(1, 3);
+// console.log(marks);
+
+// arrays/ values reverse
+// marks.reverse();
+// console.log(marks);
